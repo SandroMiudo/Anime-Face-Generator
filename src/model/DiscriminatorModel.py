415e8885e8ee8f81b601065cd7be00776a1de57c
SandroMiudo <sandromiudo0@gmail.com>
2024-07-13 23:56:29 +0200

    Extended model with layers + its call and compute loss methods

--- src/model/DiscriminatorModel.py
+++ src/model/DiscriminatorModel.py
@@ -1,8 +1,9 @@
 from keras import models
 from keras import layers
+import tensorflow as tf
 
 class DiscriminatorModel(models.Model):
-    
+
     def __init__(self, input_shape:tuple[int,int,int]):
         super().__init__()
         self.input_layer  = layers.Input(input_shape)
@@ -13,4 +14,25 @@ class DiscriminatorModel(models.Model):
         self.leaky_relu_2 = layers.LeakyReLU()
         self.dropout_layer_2 = layers.Dropout(rate=0.3)
         self.flatten_layer = layers.Flatten() # out = (batch, filters)
-        self.dense_layer   = layers.Dense(1) 
+        self.dense_layer   = layers.Dense(1)
+
+    def compute_loss(self, gen_images_output, real_images):
+        real_images_true_value = tf.ones_like(real_images)
+        fake_images_true_value = tf.zeros_like(gen_images_output)
+        return self.loss(real_images_true_value, real_images) + self.loss(fake_images_true_value, gen_images_output)
+    
+    def call(self, inputs, training=None):
+        x = self.input_layer(inputs, training)
+        x = self.conv_layer_1(x, training)
+        x = self.leaky_relu_1(x, training)
+        x = self.dropout_layer_1(x, training)
+        x = self.conv_layer_2(x, training)
+        x = self.leaky_relu_2(x, training)
+        x = self.dropout_layer_2(x, training)
+        x = self.flatten_layer(x, training)
+        x = self.dense_layer(x, training)
+
+        return x
+
+    def apply_grads(self, discrimi_grads):
+        self.optimizer.apply_gradients(zip(discrimi_grads, self.trainable_variables))
