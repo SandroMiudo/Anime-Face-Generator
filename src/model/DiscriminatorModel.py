ee946406a29442e9a57f91d2a2a1e6bb580d3317
SandroMiudo <sandromiudo0@gmail.com>
2024-07-14 23:57:44 +0200

    call methods are now passing the trainable bool value to layers + minor changes

--- src/model/DiscriminatorModel.py
+++ src/model/DiscriminatorModel.py
@@ -4,10 +4,11 @@ import tensorflow as tf
 
 class DiscriminatorModel(models.Model):
 
-    def __init__(self, input_shape:tuple[int,int,int]):
+    def __init__(self, i_shape:tuple[int,int,int]):
         super().__init__()
-        self.input_layer  = layers.Input(input_shape)
-        self.conv_layer_1 = layers.Conv2D(filters=64, kernel_size=(5,5), strides=(2,2))
+        self.conv_layer_1 = layers.Conv2D(filters=64, kernel_size=(5,5), strides=(2,2),
+            input_shape=i_shape)
+        self.i_shape = i_shape
         self.leaky_relu_1 = layers.LeakyReLU()
         self.dropout_layer_1 = layers.Dropout(rate=0.3) # rate at which input will be replaced by 0
         self.conv_layer_2 = layers.Conv2D(filters=128, kernel_size=(5,5), strides=(2,2)) 
@@ -22,17 +23,26 @@ class DiscriminatorModel(models.Model):
         return self.loss(real_images_true_value, real_images) + self.loss(fake_images_true_value, gen_images_output)
     
     def call(self, inputs, training=None):
-        x = self.input_layer(inputs, training)
-        x = self.conv_layer_1(x, training)
-        x = self.leaky_relu_1(x, training)
-        x = self.dropout_layer_1(x, training)
-        x = self.conv_layer_2(x, training)
-        x = self.leaky_relu_2(x, training)
-        x = self.dropout_layer_2(x, training)
-        x = self.flatten_layer(x, training)
-        x = self.dense_layer(x, training)
+        x = self.conv_layer_1(inputs, training=training)
+        x = self.leaky_relu_1(x, training=training)
+        x = self.dropout_layer_1(x, training=training)
+        x = self.conv_layer_2(x, training=training)
+        x = self.leaky_relu_2(x, training=training)
+        x = self.dropout_layer_2(x, training=training)
+        x = self.flatten_layer(x, training=training)
+        x = self.dense_layer(x, training=training)
 
         return x
+
+    def get_config(self):
+        return {
+            "i_shape" : self.i_shape
+        }
+
+    @classmethod
+    def from_config(cls, config):
+        input_shape = config.pop("i_shape")
+        return cls(input_shape)
 
     def apply_grads(self, discrimi_grads):
         self.optimizer.apply_gradients(zip(discrimi_grads, self.trainable_variables))
--- src/model/GeneratorModel.py
+++ src/model/GeneratorModel.py
@@ -2,44 +2,36 @@ from keras import models
 from keras import layers
 import tensorflow as tf
 
-
-
 class GeneratorModel(models.Model):
     """
     input_shape : size of the noise vector -> defaults to 164
     """
-    def __init__(self, input_shape:int=164, higher_images_resolution=True):
+    def __init__(self, i_shape:int=164, higher_images_resolution=True):
         super().__init__()
-        self.noise_vector_shape = input_shape
+        self.noise_vector_shape = i_shape
         self.higher_images_resolution = higher_images_resolution
-        self.input_layer = layers.Input(input_shape)
-        self.dense_layer_1 = layers.Dense(8*8*256, use_bias=False)
+        self.dense_layer_1 = layers.Dense(8*8*256, use_bias=False, 
+            input_shape=i_shape)
         self.batch_normalize_layer_1 = layers.BatchNormalization()
         self.leaky_relu_1 = layers.LeakyReLU()
 
         self.reshape_layer_1 = layers.Reshape((8, 8, 256))
-        assert self.output_shape == (None, 8, 8, 256) # Note: None is the batch size
 
         self.conv_layer_1 = layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False)
-        assert self.output_shape == (None, 8, 8, 128)
         self.batch_normalize_layer_2 = layers.BatchNormalization()
         self.leaky_relu_2 = layers.LeakyReLU()
 
         self.conv_layer_2 = layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False)
-        assert self.output_shape == (None, 16, 16, 64)
         self.batch_normalize_layer_3 = layers.BatchNormalization()
         self.leaky_relu_3 = layers.LeakyReLU()
 
         if higher_images_resolution: # (128,128)
             self.conv_layer_3 = layers.Conv2DTranspose(32, (5, 5), strides=(4, 4), padding='same', use_bias=False)
-            assert self.output_shape == (None, 64, 64, 32)
             self.batch_normalize_layer_4 = layers.BatchNormalization()
             self.leaky_relu_4 = layers.LeakyReLU()
             self.conv_layer_4 = layers.Conv2DTranspose(3, (5,5), strides=(2,2), padding='same', use_bias=False, activation='tanh')
-            assert self.output_shape == (None, 128, 128 , 3)
         else: # (64,64)
             self.conv_layer3 = layers.Conv2DTranspose(3, (5, 5), strides=(4, 4), padding='same', use_bias=False, activation='tanh')
-            assert self.output_shape == (None, 64, 64, 3)
 
     def compute_loss(self, gen_images_output):
         fake_images_true_value = tf.ones_like(gen_images_output)
@@ -49,24 +41,40 @@ class GeneratorModel(models.Model):
         return self.noise_vector_shape
 
     def call(self, inputs, training=None):
-        x = self.input_layer(inputs)
-        x = self.dense_layer_1(x, training)
-        x = self.batch_normalize_layer_1(x, training)
-        x = self.leaky_relu_1(x, training)
-        x = self.reshape_layer_1(x, training)
-        x = self.conv_layer_1(x, training)
-        x = self.batch_normalize_layer_2(x, training)
-        x = self.leaky_relu_2(x, training)
-        x = self.conv_layer_2(x, training)
-        x = self.batch_normalize_layer_3(x, training)
-        x = self.leaky_relu_3(x, training)
+        x = self.dense_layer_1(inputs, training=training)
+        x = self.batch_normalize_layer_1(x, training=training)
+        x = self.leaky_relu_1(x, training=training)
+        x = self.reshape_layer_1(x, training=training)
+        x = self.conv_layer_1(x, training=training)
+        x = self.batch_normalize_layer_2(x, training=training)
+        x = self.leaky_relu_2(x, training=training)
+        x = self.conv_layer_2(x, training=training)
+        x = self.batch_normalize_layer_3(x, training=training)
+        x = self.leaky_relu_3(x, training=training)
         if(self.higher_images_resolution):
-            x = self.conv_layer_3(x, training)
-            x = self.batch_normalize_layer_4(x, training)
-            x = self.leaky_relu_4(x, training)
-            x = self.conv_layer_4(x, training)
+            x = self.conv_layer_3(x, training=training)
+            x = self.batch_normalize_layer_4(x, training=training)
+            x = self.leaky_relu_4(x, training=training)
+            x = self.conv_layer_4(x, training=training)
         else:
-            x = self.conv_layer3(x, training)
+            x = self.conv_layer3(x, training=training)
+
+        return x
     
+    def get_config(self):
+        input_shape = self.noise_vector_shape
+        higher_res  = self.higher_images_resolution
+
+        return {
+            "i_shape" : input_shape, 
+            "higher_images_resolution" : higher_res
+        }
+
+    @classmethod
+    def from_config(cls, config):
+        input_shape       =  config.pop("i_shape")
+        higher_images_res = config.pop("higher_images_resolution")
+        return cls(input_shape, higher_images_res)
+
     def apply_grads(self, gen_grads):
         self.optimizer.apply_gradients(zip(gen_grads, self.trainable_variables))
