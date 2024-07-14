4018a6f96aa57a264fcd85146b14181556e7d4e6
SandroMiudo <sandromiudo0@gmail.com>
2024-07-14 23:56:36 +0200

    included checkpoint managing on the model + callback list now invoked on train batch (begin & start) + some other minor changes

--- src/model/BaseModel.py
+++ src/model/BaseModel.py
@@ -1,9 +1,9 @@
 from keras import models
 from keras import saving
 import tensorflow as tf
-import GeneratorModel as gen_model, DiscriminatorModel as disc_model
+from .  import GeneratorModel as gen_model, DiscriminatorModel as disc_model
 from keras import callbacks
-from ..callbacks import GeneratorCallback as gen_callback
+from callbacks import GeneratorCallback as gen_callback
 from os import path
 
 @saving.register_keras_serializable()
@@ -15,15 +15,34 @@ class BaseModel(models.Model):
         self.generator_model = generator_model
         self.discriminator_model = discriminator_model
         self.batch_size = batch_size
-        self.callback_list = callbacks.CallbackList(add_history=True, 
-                                                    add_progbar=True)
+        self.callback_list = callbacks.CallbackList(add_history=True, add_progbar=True)
 
     def get_config(self):
-        return super().get_config()
+        base_configuration = super().get_config()
+        gen_model_serialization  = self.generator_model.get_config()
+        disc_model_serialization = self.discriminator_model.get_config()
+
+        architecture_configuration = {
+            "generator_model" : gen_model_serialization,
+            "discriminator_model" : disc_model_serialization,
+            "batch_size" : self.batch_size
+        }
+
+        return {**base_configuration, **architecture_configuration}
     
+    def generate(self, n):
+        noise_vectors = tf.random.normal([n, 
+            self.generator_model.get_noise_vector_shape()])
+        return self.generator_model(noise_vectors, training=False)
+
     @classmethod
-    def from_config(cls, config, custom_objects=None):
-        return super().from_config(config, custom_objects)
+    def from_config(cls, config):
+        gen_model_serialization  = config.pop("generator_model")
+        disc_model_serialization = config.pop("discriminator_model")
+
+        generator_model = gen_model.GeneratorModel.from_config(gen_model_serialization)
+        discriminator_model = disc_model.DiscriminatorModel.from_config(disc_model_serialization)
+        return cls(generator_model, discriminator_model, config.pop("batch_size"))
     
     def train_step(self, batch):
         noise_vector_shape = self.generator_model.get_noise_vector_shape()
@@ -51,12 +70,13 @@ class BaseModel(models.Model):
         # history callback is passed in the constructor of the callback list.
         early_stop_callback1 = callbacks.EarlyStopping("gen_loss", patience=5, mode="min", start_from_epoch=10) # stop crit for generator
         early_stop_callback2 = callbacks.EarlyStopping("disc_loss", patience=5, mode="min", start_from_epoch=10) # stop crit for discriminator
-        checkpoint_callback1 = callbacks.ModelCheckpoint(
+        """ we don't want to save each epoch -> write custom saving callback
+        checkpoint_callback1 = callbacks.ModelCheckpoint( 
             path.join("ckpt", "weights", "ckpt.weights.{epoch:02d}.h5"),
-            save_freq='epoch') # only saving weights 
+            save_freq='epoch') # only saving weights """
         checkpoint_callback2 = callbacks.ModelCheckpoint(
             path.join("ckpt", "model", "model.keras"),
-            save_freq='epoch') # saving whole model
+            save_freq='epoch', save_weights_only=False) # saving whole model
         generator_callback = gen_callback.GeneratorCallback()
 
         if(with_early_stop):
@@ -65,28 +85,39 @@ class BaseModel(models.Model):
 
         if(generate_while_training):
             self.callback_list.append(generator_callback)
-        self.callback_list.append(checkpoint_callback1)
+        # self.callback_list.append(checkpoint_callback1)
         self.callback_list.append(checkpoint_callback2)
         
         self.callback_list.set_model(self)
-        self.callback_list.set_params({"epochs" : epochs})
+        self.callback_list.set_params({"epochs" : epochs, "verbose" : 1,
+            "steps" : len(dataset)})
+
+        self.callback_list.on_train_begin()
 
         for e in range(epochs):
-            if(self.stop_training): # early stopping criteria has been met
-                return self.history
+            #if(self.stop_training): # early stopping criteria has been met
+            #    return self.history
+            self.callback_list.on_epoch_begin(e)
+            batch_nr = 1
+
             for batch in dataset:
+                self.callback_list.on_train_batch_begin(batch_nr)
                 loss_dict = self.train_step(batch)
-                self.generator_model.metrics[0].update(loss_dict["gen_loss"])
-                self.discriminator_model[0].update(loss_dict["disc_loss"])
-            
+                self.generator_model.metrics[0].update_state(loss_dict["gen_loss"])
+                self.discriminator_model.metrics[0].update_state(loss_dict["disc_loss"])
+                self.callback_list.on_train_batch_end(batch_nr)
+                batch_nr += 1
+
             self.callback_list.on_epoch_end(e, {
                 "gen_loss" : self.generator_model.metrics[0].result(),
                 "disc_loss" : self.discriminator_model.metrics[0].result()})
         
-            self.generator_model.metrics[0].reset()
-            self.discriminator_model.metrics[0].reset()
+            self.generator_model.metrics[0].reset_state()
+            self.discriminator_model.metrics[0].reset_state()
 
-        return self.history    
+        self.callback_list.on_train_end()
+
+        return self.history
 
     def evaluate(self, quantity:int=1000):
         noise_vector_shape = self.generator_model.get_noise_vector_shape()
@@ -115,3 +146,7 @@ class BaseModel(models.Model):
         noise_vector_shape = self.generator_model.get_noise_vector_shape()
         noise_vectors = tf.random.normal([quantity, noise_vector_shape])
         return self.generator_model(noise_vectors, training=False)
+    
+    def checkpoint(self, _ckpt):
+        path_str = path.join("ckpt", f"ckpt_{_ckpt}.weights.h5")
+        self.save_weights(path_str, overwrite=True)
