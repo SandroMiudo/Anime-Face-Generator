3227b4a271b74a5915f821193f1b63ecd85074b1
SandroMiudo <sandromiudo0@gmail.com>
2024-07-16 13:31:47 +0200

    bug fix : do not checkpoint in epoch 0

--- src/callbacks/CheckpointCallback.py
+++ src/callbacks/CheckpointCallback.py
@@ -2,13 +2,13 @@ from keras import callbacks
 
 class CheckpointCallback(callbacks.Callback):
 
-    def __init__(self, checkpoint_on_epoch=5):
+    def __init__(self, checkpoint_on_epoch=10):
         super().__init__()
         self.checkpoint_on_epoch = checkpoint_on_epoch
         self._checkpoint = 0
 
     def on_epoch_end(self, epoch, logs=None):
-        if(epoch % self.checkpoint_on_epoch == 0):
+        if(epoch % self.checkpoint_on_epoch == 0 and epoch != 0):
             print("checkpoint reached ...")
             self.model.checkpoint(self._checkpoint)
             self._checkpoint += 1
