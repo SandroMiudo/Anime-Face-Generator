b3b41e03baeb2de3e249497aaa31fc61570cb5bd
SandroMiudo <sandromiudo0@gmail.com>
2024-07-26 16:49:01 +0200

    extended callback with on epoch end function, to show the current learning rate  -> useful when using learning rate schedules

--- src/callbacks/ModelCallback.py
+++ src/callbacks/ModelCallback.py
@@ -1,10 +1,14 @@
 from keras.callbacks import Callback
-from keras import utils
+from keras import optimizers
 from os import path
 
 class ModelCallback(Callback):
     def __init__(self):
         super().__init__()
+
+    def set_params(self, params):
+        self.epochs = params["epochs"]
+        self.steps  = params["steps"]
 
     def on_train_begin(self, logs=None):
         path_to_file = path.join("media", "model_summary.txt")
@@ -18,3 +22,14 @@ class ModelCallback(Callback):
         model = self.model
         model(None) # build model
         self.model.apply_summary(write_fn)
+
+    def on_epoch_end(self, epoch, logs=None):
+        disc_dict = self.model.discriminator
+        gen_dict  = self.model.generator
+
+        d_learning = disc_dict["learning_rate"]
+        g_learning = gen_dict["learning_rate"]
+
+        # just useful for learning rate schedulers
+        print(f"Learning rate discriminator at epoch {epoch+1} = {d_learning}")
+        print(f"Learning rate generator at epoch {epoch+1} = {g_learning}")
