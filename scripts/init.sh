b54bad3383282e9d91d82fc4214b80df56e9cb2a
SandroMiudo <sandromiudo0@gmail.com>
2024-08-01 12:55:02 +0200

    added init script

---
+++ scripts/init.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+[ ! -f ~/.alias ] && touch ~/.alias
+
+e=$(grep -e "ENTRY=.*" ~/.alias)
+l=$(grep -e "LOAD=*" ~/.alias)
+i=$(grep -e "INF=*" ~/.alias)
+t=$(grep -e "TRAIN=*" ~/.alias)
+
+[ -z "${e}" ] && tee --append ~/.alias &>/dev/null <<< "alias ENTRY=python3 src/Entrypoint.py"
+[ -z "${l}" ] && chmod u+x scripts/load.sh && tee --append ~/.alias <<< "alias LOAD=scripts/load.sh" &>/dev/null
+[ -z "${i}" ] && chmod u+x scripts/inference.sh && tee --append ~/.alias <<< "alias INF=./scripts/inference.sh" &>/dev/null
+[ -z "${t}" ] && chmod u+x scripts/train.sh && tee --append ~/.alias <<< "alias TRAIN=./scripts/train.sh" &>/dev/null
