cf21b6d709b5c7f0adb5d71f53b8e4703eadbd59
SandroMiudo <sandromiudo0@gmail.com>
2024-08-04 16:39:45 +0200

    bug fix

--- scripts/load.sh
+++ scripts/load.sh
@@ -6,7 +6,7 @@
 printf "generate images per epochs = %d -- total epochs = %d --\
  no data augmentation = %d\n" "$1" "$2" "$3"
 
-cmd="python3 ${PWD}/src/EntryPoint.py --load --epochs "$1" --generate-per-epoch "$2" --no-augment"
+cmd="python3 ${PWD}/src/EntryPoint.py --load --epochs "$2" --generate-per-epoch "$1" --no-augment"
 [ "$3" -eq 1 ] && cmd="${cmd%' --no-augment'}"
 
 eval "${cmd}"
