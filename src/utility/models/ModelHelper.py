cfbffb0ce42964c468e1de8eda2e7c9f527f7978
SandroMiudo <sandromiudo0@gmail.com>
2024-08-05 19:02:06 +0200

    changed both construct functions -> less layers in total

--- src/utility/models/ModelHelper.py
+++ src/utility/models/ModelHelper.py
@@ -1,21 +1,14 @@
-from typing import Any
 from keras import layers
 
 class Conv2DBlockBuilder:
 
     @staticmethod
-    def construct(filters:int, kernel_size:tuple[int, int], i_shape:int|None=None) -> list[layers.Layer]:
+    def construct(filters:int, kernel_size:tuple[int, int]) -> list[layers.Layer]:
         block = []
-        if i_shape == None:
-            block.append(layers.Conv2D(filters, kernel_size, padding='same', activation='relu',
-                kernel_initializer='he_normal'))
-        else:
-            block.append(layers.Conv2D(filters, kernel_size, padding='same', activation='relu',
-                kernel_initializer='he_normal', input_shape=i_shape))
+        block.append(layers.Conv2D(filters, kernel_size, padding='same', 
+            activation='leaky_relu'))
         block.append(layers.BatchNormalization())
-        block.append(layers.Conv2D(filters, kernel_size, padding='same', activation='relu',
-            kernel_initializer='he_normal'))
-        block.append(layers.BatchNormalization())
+        block.append(layers.AveragePooling2D(pool_size=(2,2)))
 
         return block
     
@@ -24,10 +17,11 @@ class Conv2D_T_BlockBuilder:
     @staticmethod
     def construct(filters:int, kernel_size:tuple[int, int]) -> list[layers.Layer]:
         block = []
-        block.append(layers.Conv2DTranspose(filters=filters, kernel_size=kernel_size, strides=(2,2),
-                    padding='same'))
-        block.append(layers.Dropout(0.1))
-        block.extend(Conv2DBlockBuilder.construct(filters, kernel_size))
+        block.append(layers.Conv2DTranspose(filters, kernel_size, (2,2),
+            padding='same'))
+        block.append(layers.Conv2D(filters, (5,5), padding='same', 
+            activation='leaky_relu'))
+        block.append(layers.BatchNormalization())
 
         return block
     
