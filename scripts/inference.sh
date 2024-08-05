0d52237052437caf8ce905a75f66a6a4669b28b0
SandroMiudo <sandromiudo0@gmail.com>
2024-08-05 19:31:21 +0200

    updated scripts -> now receiving target argument

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-[ "$#" -ne 1 ] && printf "Expected one argument, but received %d\n" "$#" && exit 1;
+[ "$#" -ne 2 ] && printf "Expected two arguments, but received %d\n" "$#" && exit 1;
 
 printf "Starting inference -- Generating %d images ...\n" "$1"
 
-python3 src/EntryPoint.py --inference --inference-count "$1"
+python3 src/EntryPoint.py --inference --inference-count "$1" --target "$2"
--- scripts/load.sh
+++ scripts/load.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
-[ "$#" -ne 3 ] && printf "Exptected two positional arguments but only received\
+[ "$#" -ne 4 ] && printf "Exptected four positional arguments but received\
  $#. Aborting ...\n" && exit 1
 
 printf "generate images per epochs = %d -- total epochs = %d --\
- no data augmentation = %d\n" "$1" "$2" "$3"
+ no data augmentation = %d\n -- target = %d\n" "$1" "$2" "$3" "$4"
 
-cmd="python3 ${PWD}/src/EntryPoint.py --load --epochs "$2" --generate-per-epoch "$1" --no-augment"
+cmd="python3 ${PWD}/src/EntryPoint.py --load --epochs $2\
+ --generate-per-epoch $1 --target $4 --no-augment"
 [ "$3" -eq 1 ] && cmd="${cmd%' --no-augment'}"
 
 eval "${cmd}"
