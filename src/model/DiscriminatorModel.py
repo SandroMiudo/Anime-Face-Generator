a2d74b8f3118b95559c9fecf8bd090d0e2548b00
SandroMiudo <sandromiudo0@gmail.com>
2024-08-05 19:10:44 +0200

    Renewed init function, to now use fewer layers + support for multiple resolutions Added label smoothing when computing loss Add Artificial noise to discriminators inputs -> clip values to [0,1] Adapted call function, to use new architecture

--- src/model/DiscriminatorModel.py
+++ src/model/DiscriminatorModel.py
@@ -1,58 +1,74 @@
 from keras import models
 from keras import layers
-from keras import initializers
 from keras import metrics
-from keras import regularizers
 from keras import optimizers
 from keras import losses
 import tensorflow as tf
 from keras import saving
-from utility.models import ModelHelper
+from utility.models.ModelHelper import Conv2DBlockBuilder, LayerIterator
+from utility.enums.defs import ImageResolution
+from utility.enums.defs import ImageResolutionOps
 
 class DiscriminatorModel(models.Model):
 
     def __init__(self, i_shape:tuple[int,int,int], learning_rate):
         super().__init__()
         self._i_shape = i_shape
-        self._bin_loss = losses.BinaryCrossentropy(label_smoothing=0.1)
+        self._bin_loss = losses.BinaryCrossentropy()
         self._loc_optimizer = optimizers.Adam(learning_rate)
         self._loss_tracker = metrics.Mean(name="loss")
-        self._conv_block_1 = ModelHelper.Conv2DBlockBuilder.construct(16, (3,3), 
-            i_shape=self._i_shape)
-        self._pool_layer_1 = layers.MaxPooling2D(pool_size=(2,2))
-        self._drop_layer_1 = layers.Dropout(0.1)
-        self._conv_block_2 = ModelHelper.Conv2DBlockBuilder.construct(32, (3,3))
-        self._pool_layer_2 = layers.MaxPooling2D(pool_size=(2,2))
-        self._drop_layer_2 = layers.Dropout(0.1)
-        self._conv_block_3 = ModelHelper.Conv2DBlockBuilder.construct(64, (3,3))
-        self._pool_layer_3 = layers.MaxPooling2D(pool_size=(2,2))
-        self._drop_layer_3 = layers.Dropout(0.1)
-        self._conv_block_4 = ModelHelper.Conv2DBlockBuilder.construct(128, (3,3))
+
+        _kernel_c = (5,5)
+
+        self._conv_block_1 = Conv2DBlockBuilder.construct(32, _kernel_c)
+        self._conv_block_2 = Conv2DBlockBuilder.construct(64, _kernel_c)
+
+        if ImageResolutionOps.tgt_in(ImageResolution.x_96_96):
+            self._conv_block_3 = Conv2DBlockBuilder.construct(96, _kernel_c)
+
+        if ImageResolutionOps.tgt_in(ImageResolution.x_112_112):
+            self._conv_block_3 = Conv2DBlockBuilder.construct(112, _kernel_c)
+
+        if ImageResolutionOps.tgt_in(ImageResolution.x_128_128):
+            self._conv_block_3 = Conv2DBlockBuilder.construct(128, _kernel_c)
+
+        if ImageResolutionOps.tgt_in(ImageResolution.x_256_256):
+            self._conv_block_4 = Conv2DBlockBuilder.construct(256, _kernel_c)
+
+        if ImageResolutionOps.tgt_in(ImageResolution.x_512_512):
+            self._conv_block_5 = Conv2DBlockBuilder.construct(512, _kernel_c)
 
         self._flatten_layer = layers.Flatten()
-        self._dense_layer_1 = layers.Dense(8, activation='relu', 
+        self._dense_layer_1 = layers.Dense(128, activation='leaky_relu', 
             kernel_initializer='he_normal')
         self._dense_layer_2 = layers.Dense(1, activation='sigmoid',
             kernel_initializer='glorot_normal')
 
     def compute_loss(self, gen_images_output, real_images):
-        real_images_true_value = tf.ones_like(real_images)
-        fake_images_true_value = tf.zeros_like(gen_images_output)
+        real_images_true_value = tf.ones_like(real_images) * 0.9
+        fake_images_true_value = tf.zeros_like(gen_images_output) + 0.1
 
         return self._bin_loss(real_images_true_value, real_images) + self._bin_loss(fake_images_true_value, gen_images_output)
 
     def call(self, inputs, training=None):
-        x = ModelHelper.LayerIterator(self._conv_block_1)(inputs, training=training)
-        x = self._pool_layer_1(x, training=training)
-        x = self._drop_layer_1(x, training=training)
-        x = ModelHelper.LayerIterator(self._conv_block_2)(x, training=training)
-        x = self._pool_layer_2(x, training=training)
-        x = self._drop_layer_2(x, training=training)
-        x = ModelHelper.LayerIterator(self._conv_block_3)(x, training=training)
-        x = self._pool_layer_3(x, training=training)
-        x = self._drop_layer_3(x, training=training)
-        x = ModelHelper.LayerIterator(self._conv_block_4)(x, training=training)
-        x = self._flatten_layer(x, training=training)
+        _noise = tf.random.normal(list(inputs.shape), 0, 0.2) # potentialy increase this to 0.15 - 0.2
+        inputs = tf.clip_by_value(inputs + _noise, 0.0, 1.0)
+
+        x = LayerIterator(self._conv_block_1)(inputs, training)
+        x = LayerIterator(self._conv_block_2)(x, training)
+
+        if ImageResolutionOps.tgt_in(ImageResolution.x_96_96) or \
+           ImageResolutionOps.tgt_in(ImageResolution.x_112_112) or \
+           ImageResolutionOps.tgt_in(ImageResolution.x_128_128):
+            x = LayerIterator(self._conv_block_3)(x, training)
+
+        if ImageResolutionOps.tgt_in(ImageResolution.x_256_256):
+            x = LayerIterator(self._conv_block_4)(x, training)
+        
+        if ImageResolutionOps.tgt_in(ImageResolution.x_512_512):
+            x = LayerIterator(self._conv_block_5)(x, training)
+
+        x = self._flatten_layer(x)
         x = self._dense_layer_1(x, training=training)
         x = self._dense_layer_2(x, training=training)
         return x
